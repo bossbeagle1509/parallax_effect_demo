78e60c60f0b37ca95b99a0ca7489f16f3cf3ed29
bossbeagle1509 <bossbeagle1509@gmail.com>
2020-12-23 21:45:34 +0530

    Fixed crude approach to dealing with height of Container in ListView

--- lib/home.dart
+++ lib/home.dart
@@ -16,8 +16,10 @@ class _HomeState extends State<Home> {
 
   final GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey<ScaffoldState>();
 
-  double top1 = 120;
-  double top2 = 330;
+  double top1 = 220;
+  double top2 = 430;
+
+  double actualHeight = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -31,66 +33,76 @@ class _HomeState extends State<Home> {
               top1 = top1 - v.scrollDelta / 2;
               top2 = top2 - v.scrollDelta / 1;
             });
-          } 
+          }
         },
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Positioned(
-              top: top1,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 50.0),
-                child: Container(
-                  width: 500,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    border: Border.all(width: 10.0),
-                    borderRadius: BorderRadius.all(Radius.circular(10.0) //
-                        ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 50, horizontal: 30),
-                    child: Text(
-                      'Box 1',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              top: top2,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 40.0, bottom: 30),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    border: Border.all(width: 10.0),
-                    borderRadius: BorderRadius.all(Radius.circular(10.0) //
-                        ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 70, horizontal: 30),
-                    child: Text(
-                      'Notice how this goes up futher than the previous box',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            ListView(
+        child: LayoutBuilder(
+          builder: (ctx, constraints) {
+
+            double tempHeight = constraints.maxHeight + 500;
+
+            actualHeight = tempHeight;
+
+            return Stack(
+              alignment: Alignment.center,
               children: [
-                Container(
-                  height: 1500,
-                  color: Colors.transparent,
+                Positioned(
+                  top: top1,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 50.0),
+                    child: Container(
+                      width: 500,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        border: Border.all(width: 10.0),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0) //
+                            ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 50, horizontal: 30),
+                        child: Text(
+                          'Box 1',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: top2,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 40.0, bottom: 30),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        border: Border.all(width: 10.0),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0) //
+                            ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 70, horizontal: 30),
+                        child: Text(
+                          'Notice how this goes up futher than the previous box',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                ListView(
+                  children: [
+                    Container(
+                      height: actualHeight,
+                      // height: 1400,
+                      color: Colors.transparent,
+                    ),
+                  ],
                 ),
               ],
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
